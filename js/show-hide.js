6defb21540cf20d93045a507cf1022324bb0447b
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-30 22:16:25 +0200

    content.classList.toggle("content-hidden");

--- js/show-hide.js
+++ js/show-hide.js
@@ -6,10 +6,17 @@ console.log(content);
 
 button.addEventListener("click", function () {
   console.log("click!!!");
+  //------------VERSION-1------------------
+  //   content.classList.toggle("content-hidden");
 
-  content.classList.toggle("content-hidden");
+  //   if (content.classList.contains("content-hidden")) {
+  //     button.textContent = "Open bloc";
+  //   } else {
+  //     button.textContent = "Close bloc";
+  //   }
 
-  if (content.classList.contains("content-hidden")) {
+  //------------VERSION-2-------------
+  if (content.classList.toggle("content-hidden")) {
     button.textContent = "Open bloc";
   } else {
     button.textContent = "Close bloc";
