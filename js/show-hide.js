d55dbef67044f72af0c3b1eaa074c50fb1a73503
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-30 22:10:18 +0200

    if (content.classList.contains("content-hidden")) {

--- js/show-hide.js
+++ js/show-hide.js
@@ -1,10 +1,17 @@
 // console.log('tralala');
 const button = document.querySelector("#button");
 console.log(button);
-const content= document.querySelector("#content");
+const content = document.querySelector("#content");
 console.log(content);
 
-button.addEventListener("click",function(){
-    console.log('click!!!');
-})
+button.addEventListener("click", function () {
+  console.log("click!!!");
 
+  content.classList.toggle("content-hidden");
+
+  if (content.classList.contains("content-hidden")) {
+    button.textContent = "Open bloc";
+  } else {
+    button.textContent = "Close bloc";
+  }
+});
