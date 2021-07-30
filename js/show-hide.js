2b6b03eea1be8ad1cbe5d71ac5f1b7da5992e1ee
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-30 21:58:05 +0200

    const content= document.querySelector("#content");

--- js/show-hide.js
+++ js/show-hide.js
@@ -1,3 +1,10 @@
 // console.log('tralala');
-
 const button = document.querySelector("#button");
+console.log(button);
+const content= document.querySelector("#content");
+console.log(content);
+
+button.addEventListener("click",function(){
+    console.log('click!!!');
+})
+
