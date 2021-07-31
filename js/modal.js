fbb2a318b33c692d90c7bf22c4fdde1260b0bdb4
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-31 19:34:09 +0200

    buttonClose.addEventListener('click',function(){

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,15 @@
-console.log('Modal-js');
+// console.log('Modal-js');
+const button= document.querySelector('[data-modal-button]')
+// console.log(button);
+const modal= document.querySelector('[data-modal]')
+// console.log('Click!!!');
+const buttonClose=document.querySelector('[data-modal-close]')
+
+button.addEventListener('click',function(){
+    modal.classList.remove('hidden')
+})
+
+
+buttonClose.addEventListener('click',function(){
+        // console.log('Click on close button');
+})
