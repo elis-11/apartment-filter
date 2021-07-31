3b4e16aa988dfbed9ea86bb7a5422d7bd6d6a152
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-31 22:43:36 +0200

    modal.classList.add("hidden")

--- js/modal.js
+++ js/modal.js
@@ -49,5 +49,27 @@
 
 //-----VERSION-3-------
 // to find nessesery elements (Open and Close)
+const modalButtons = document.querySelectorAll('[data-modal-button]')
+// console.log(modalButtons);
+const modalCloseButtons= document.querySelectorAll('[data-modal-close]')
 
-// close modal
+// to open the modal
+modalButtons.forEach(function (item){
+  item.addEventListener("click", function(){
+
+    const modalId=this.dataset.modalButton
+    console.log(modalId);
+    
+    const modal= document.querySelector('#' + modalId)
+    // console.log(modal);
+    modal.classList.remove('hidden')
+  })
+})
+//to close modal
+modalCloseButtons.forEach(function (item){
+  item.addEventListener("click", function (){
+   const modal= this.closest('[data-modal]')
+  //  console.log(modal );
+   modal.classList.add("hidden")
+  })
+})
