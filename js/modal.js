7a3b67c458f556f283da2838fc4847ee8b01b3f0
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-31 19:41:53 +0200

    //-----VERSION-1-------

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,39 @@
-// console.log('Modal-js');
-const button= document.querySelector('[data-modal-button]')
+// //-----VERSION-1-------
+// // to find nessesery elements (Open and Close)
+// const button = document.querySelector("[data-modal-button]");
+// // console.log(button);
+// const modal = document.querySelector("[data-modal]");
+// // console.log('Click!!!');
+// const buttonClose = document.querySelector("[data-modal-close]");
+
+// // open modal
+// button.addEventListener("click", function () {
+//   modal.classList.remove("hidden");
+// });
+
+// // close modal
+// buttonClose.addEventListener("click", function () {
+//   // console.log('Click on close button');
+//   modal.classList.add("hidden");
+
+// });
+
+//-----VERSION-1-------
+// to find nessesery elements (Open and Close)
+const button = document.querySelector("[data-modal-button]");
 // console.log(button);
-const modal= document.querySelector('[data-modal]')
+const modal = document.querySelector("[data-modal]");
 // console.log('Click!!!');
-const buttonClose=document.querySelector('[data-modal-close]')
+const buttonClose = document.querySelector("[data-modal-close]");
 
-button.addEventListener('click',function(){
-    modal.classList.remove('hidden')
-})
+// open modal
+button.addEventListener("click", function () {
+  modal.classList.remove("hidden");
+});
 
+// close modal
+buttonClose.addEventListener("click", function () {
+  // console.log('Click on close button');
+  modal.classList.add("hidden");
 
-buttonClose.addEventListener('click',function(){
-        // console.log('Click on close button');
-})
+});
