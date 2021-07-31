03b31eef9205a1199b602b07ad2e6a739e7850a8
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-31 23:01:11 +0200

    <div id="modal-1" data-modal class="fade-block hidden">

--- js/modal.js
+++ js/modal.js
@@ -49,27 +49,38 @@
 
 //-----VERSION-3-------
 // to find nessesery elements (Open and Close)
-const modalButtons = document.querySelectorAll('[data-modal-button]')
+const modalButtons = document.querySelectorAll("[data-modal-button]");
 // console.log(modalButtons);
-const modalCloseButtons= document.querySelectorAll('[data-modal-close]')
+const modalCloseButtons = document.querySelectorAll("[data-modal-close]");
+const allModals = document.querySelectorAll("[data-modal]");
 
 // to open the modal
-modalButtons.forEach(function (item){
-  item.addEventListener("click", function(){
-
-    const modalId=this.dataset.modalButton
-    console.log(modalId);
-    
-    const modal= document.querySelector('#' + modalId)
+modalButtons.forEach(function (item) {
+  item.addEventListener("click", function () {
+    const modalId = this.dataset.modalButton;
+    // console.log(modalId);
+    const modal = document.querySelector("#" + modalId);
     // console.log(modal);
-    modal.classList.remove('hidden')
-  })
-})
+    modal.classList.remove("hidden");
+//to find inside modal block and to forbit him to give clicks
+    modal.querySelector('.modal-window').addEventListener("click", function (e) {
+      e.stopPropagation();
+    })
+  });
+});
 //to close modal
-modalCloseButtons.forEach(function (item){
-  item.addEventListener("click", function (){
-   const modal= this.closest('[data-modal]')
-  //  console.log(modal );
-   modal.classList.add("hidden")
-  })
-})
+modalCloseButtons.forEach(function (item) {
+  item.addEventListener("click", function () {
+    const modal = this.closest("[data-modal]");
+    //  console.log(modal );
+    modal.classList.add("hidden");
+  });
+});
+//to close modal on FADE
+allModals.forEach(function (item) {
+  item.addEventListener("click", function () {
+    this.classList.add("hidden");
+
+    //  console.log(modal );
+  });
+});
