7da477170209884a7ee4807da0868fdb500674a5
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-31 12:05:55 +0200

    <div class="list-group-item list-content hidden">

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,10 +1,10 @@
 console.log('tralala');
 const headers= document.querySelectorAll('[data-name="accordeon-title"]')
-console.log(headers);
+// console.log(headers);
 
 headers.forEach(function(item) {
-    console.log(item);
+    // console.log(item);
     item.addEventListener('click', function(){
-        console.log('click!!!'); 
+        // console.log('click!!!'); 
     })
 })
