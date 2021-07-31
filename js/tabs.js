782eeae0a2d16f9d62009d71e72416900aa9876d
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-31 17:03:29 +0200

    tabHeaders.forEach(function (item) {

--- js/tabs.js
+++ js/tabs.js
@@ -1,1 +1,16 @@
-console.log('tabs!!!!');
+// console.log('tabs!!!!');
+// founf all tab-headers for data-atributs
+const tabHeaders = document.querySelectorAll("[data-tab]");
+console.log(tabHeaders);
+
+tabHeaders.forEach(function (item) {
+  // console.log(item)
+  item.addEventListener("click", function () {
+    // console.log('Tab click!!!');
+    // console.log(this);
+    console.log(this.dataset.tab);
+
+    const contentBox = document.querySelector("#" + this.dataset.tab);
+    console.log(contentBox);
+  })
+})
