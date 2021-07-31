8534b03775f19fac9ba07b26567cbbea2fb72d7d
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-31 11:59:53 +0200

    item.addEventListener('click', function(){

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,1 +1,10 @@
 console.log('tralala');
+const headers= document.querySelectorAll('[data-name="accordeon-title"]')
+console.log(headers);
+
+headers.forEach(function(item) {
+    console.log(item);
+    item.addEventListener('click', function(){
+        console.log('click!!!'); 
+    })
+})
