65e7f74e06e072b6d240235d55ed846a98ba3261
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-31 19:07:34 +0200

    item.addEventListener('click', function () {

--- js/tabs.js
+++ js/tabs.js
@@ -1,24 +1,46 @@
-// founf all tab-headers for data-atributs
-const tabHeaders = document.querySelectorAll("[data-tab]");
-// console.log(tabHeaders);
-// found all contentBoxes
-const contentBoxes = document.querySelectorAll("[data-tab-content]");
+// // founf all tab-headers for data-atributs
+// const tabHeaders = document.querySelectorAll("[data-tab]");
+// // console.log(tabHeaders);
+// // found all contentBoxes
+// const contentBoxes = document.querySelectorAll("[data-tab-content]");
+
+// tabHeaders.forEach(function (item) {
+//   // console.log(item)
+//   item.addEventListener("click", function () {
+//     // console.log('Tab click!!!');
+//     // console.log(this);
+//     // console.log(this.dataset.tab);
+
+//     // -1- to hide all contentBoxes
+//     contentBoxes.forEach(function (item) {
+//       item.classList.add("hidden");
+//     });
+
+//     // -2- to select the contentBox and to show it
+//     const contentBox = document.querySelector("#" + this.dataset.tab);
+//     // console.log(contentBox);
+//     contentBox.classList.remove("hidden");
+//   });
+// });
+
+
+
+// Нашли все заголовки табов по data атрибуту
+const tabHeaders = document.querySelectorAll('[data-tab]');
+// Нашли все контент боксы
+const contentBoxes = document.querySelectorAll('[data-tab-content]');
 
 tabHeaders.forEach(function (item) {
-  // console.log(item)
-  item.addEventListener("click", function () {
-    // console.log('Tab click!!!');
-    // console.log(this);
-    // console.log(this.dataset.tab);
+    item.addEventListener('click', function () {
 
-    // -1- to hide all contentBoxes
-    contentBoxes.forEach(function (item) {
-      item.classList.add("hidden");
-    });
+        // 1. Скрыть все content box
+        contentBoxes.forEach(function (item) {
+            item.classList.add('hidden');
+        });
 
-    // -2- to select the contentBox and to show it
-    const contentBox = document.querySelector("#" + this.dataset.tab);
-    // console.log(contentBox);
-    contentBox.classList.remove("hidden");
-  });
-});
+        // 2. Выбрать нужный content box и показать его
+        const contentBox = document.querySelector('#' + this.dataset.tab);
+        contentBox.classList.remove('hidden');
+
+    })
+})
