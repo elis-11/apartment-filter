cb85e41c48e5c9fd0a9be936157dd29d4efead26
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-31 17:17:07 +0200

    item.classList.add('hidden')

--- js/tabs.js
+++ js/tabs.js
@@ -2,15 +2,26 @@
 // founf all tab-headers for data-atributs
 const tabHeaders = document.querySelectorAll("[data-tab]");
 console.log(tabHeaders);
+// found all contentBoxes
+const contentBoxes=document.querySelectorAll('[data-tab-content]');
+
 
 tabHeaders.forEach(function (item) {
   // console.log(item)
   item.addEventListener("click", function () {
     // console.log('Tab click!!!');
     // console.log(this);
-    console.log(this.dataset.tab);
+    // console.log(this.dataset.tab);
 
     const contentBox = document.querySelector("#" + this.dataset.tab);
-    console.log(contentBox);
+    // console.log(contentBox);
+
+    // -1- to find the content box
+    contentBoxes.forEach(function (item){
+        item.classList.add('hidden')
+    })
+
+
+    // -2- to select the content boxand to show it
   })
 })
