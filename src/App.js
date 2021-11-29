454c469a14e102bdcd7da23beebff7eac7f4f9c1
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-29 20:36:33 +0100

    it works

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.scss';
 
 export const App = () => {
-	
 	const [ name, setName ] = useState('');
-	
-	const [ phone, setPhone ] = useState('');
-	
-	const [ payload, setPayload ] = useState({});
+	const [ nameIsValid, setNameIsValid ] = useState(false);
 
+	const [ phone, setPhone ] = useState('');
+	const [ phoneIsValid, setPhoneIsValid ] = useState(false);
+
+	const [ payload, setPayload ] = useState({});
+	const [ formIsValid, setFormIsValid ] = useState(false);
+
+	useEffect(() => {
+		setFormIsValid(nameIsValid && phoneIsValid);
+	}, [nameIsValid, phoneIsValid])
 
 	const handleName = (e) => {
 		let _name = e.target.value;
+		if (_name !== '' && _name.length <= 10) {
+			setNameIsValid(true);
+		} else {
+			setNameIsValid(false);
+		}
 		setName(_name);
 	};
 
 	const handlePhone = (e) => {
 		let _phone = e.target.value;
+		if (_phone !== '' && /^\d\d\d-\d\d\d-\d\d\d\d$/.test(_phone)) {
+			setPhoneIsValid(true);
+		} else {
+			setPhoneIsValid(false);
+		}
 		setPhone(_phone);
 	};
 
-	// const clearPayload = () => {
-	// 	setPayload((prev) => ({}));
-	// };
+	const clearPayload = () => {
+		setPayload((prev) => ({}));
+	};
 	// clearPayload();
-
 
 	const handleButton = (e) => {
 		e.preventDefault();
@@ -40,20 +54,20 @@ export const App = () => {
 			<form>
 				<fieldset>
 					<legend>Order Form</legend>
-					<div className="row">
+					<div className={'row ' + (nameIsValid ? 'valid' : 'invalid')}>
 						<label htmlFor="name">Name</label>
 						<input type="text" id="name" value={name} onChange={handleName} />
 					</div>
-					<div className="note">required, maximum 10 characters</div>
+					<div className={'note ' + (nameIsValid ? 'valid' : 'invalid')}>required, maximum 10 characters</div>
 
-					<div className="row">
+					<div className={'row ' + (phoneIsValid ? 'valid' : 'invalid')}>
 						<label htmlFor="phone">Phone</label>
 						<input type="text" id="phone" value={phone} onChange={handlePhone} />
 					</div>
-					<div className="note">e.g. 555-333-2222</div>
+					<div className={'note ' + (phoneIsValid ? 'valid' : 'invalid')}>e.g. 555-333-2222</div>
 
 					<div className="buttonRow">
-						<button onClick={handleButton}>Register</button>
+					<button disabled={!formIsValid} onClick={handleButton}>Register</button>
 					</div>
 				</fieldset>
 			</form>
